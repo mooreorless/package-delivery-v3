04d530a87a7107f05d0fd7e4e9546c47abe48549
mooreorless <elliott.moore@moretonbay.qld.gov.au>
2016-09-21 19:57:05 +1000

    configured mlab instance on heroku

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,6 +1,9 @@
 var mongoose = require('mongoose');
 var gracefulShutdown;
-var dbURI = 'mongodb://admin:admin@ds019746.mlab.com:19746/deliverydb';
+// var dbURI = 'mongodb://admin:admin@ds019746.mlab.com:19746/deliverydb';
+
+// Heroku connection
+var dbURI = 'mongodb://admin:admin@ds035826.mlab.com:35826/heroku_1tt33bgj';
 
 if (process.env.NODE_ENV === 'production') {
   dbURI = process.env.MONGOLAB_URI;
