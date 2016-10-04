d7ecad68e22ba65ac10ddb4a7aa181bee03d2cf9
mooreorless <elliott.moore@moretonbay.qld.gov.au>
2016-10-04 15:09:13 +1000

    removed unused deps and cleaned up app.js

--- app.js
+++ app.js
@@ -1,54 +1,38 @@
-/*
-
-  There are some minor modifications to the default Express setup
-  Each is commented and marked with [SH] to make them easy to find
-
- */
-
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-// [SH] Require Passport
 var passport = require('passport');
 
-// [SH] Bring in the data model
+// Load api models and auth
 require('./app_api/models/db');
-// [SH] Bring in the Passport config after model is defined
 require('./app_api/config/passport');
 
-
-// [SH] Bring in the routes for the API (delete the default routes)
+// Load api endpoints
 var routesApi = require('./app_api/routes/index');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-// [SH] Set the app_client folder to serve static resources
-app.use(express.static(path.join(__dirname, 'app_client')));
 
-// [SH] Initialise Passport before using the route middleware
+// Serves static assets
+app.use(express.static(path.join(__dirname, 'public')));
+// Serves angular
+app.use(express.static(path.join(__dirname, 'client')));
+
+// Begin authentication service before loading routes
 app.use(passport.initialize());
 
-// [SH] Use the API routes when path starts with /api
+// Prefix database routes
 app.use('/api', routesApi);
 
-// [SH] Otherwise render the index.html page for the Angular SPA
-// [SH] This means we don't have to map all of the SPA routes in Express
+// Default route
 app.use(function(req, res) {
-  res.sendFile(path.join(__dirname, 'app_client', 'index.html'));
+  res.sendFile(path.join(__dirname, 'client', 'index.html'));
 });
 
 
@@ -59,9 +43,7 @@ app.use(function(req, res, next) {
     next(err);
 });
 
-// error handlers
-
-// [SH] Catch unauthorised errors
+// Handle auth errors
 app.use(function (err, req, res, next) {
   if (err.name === 'UnauthorizedError') {
     res.status(401);
--- gulpfile.js
+++ gulpfile.js
@@ -6,17 +6,17 @@ var sourcemaps = require('gulp-sourcemaps');
 var ngHtml2Js = require("gulp-ng-html2js");
 
 gulp.task('scripts', function() {
-  gulp.src(['./app_client/**/*.js', '!./app_client/**/*.test.js', '!./app_client/app.min.js'])
+  gulp.src(['./client/**/*.js', '!./client/**/*.test.js', '!./client/app.min.js'])
     .pipe(sourcemaps.init())
       .pipe(concat('./app.min.js'))
       .pipe(uglify({mangle: true}))
-      .pipe(gulp.dest('app_client'))
+      .pipe(gulp.dest('client'))
     .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('app_client'));
+    .pipe(gulp.dest('client'));
 });
 
 gulp.task('watch', function() {
-  watch(['./app_client/**/*.js', '!./app_client/**/*.test.js', '!./app_client/app.min.js'], function () {
+  watch(['./client/**/*.js', '!./client/**/*.test.js', '!./client/app.min.js'], function () {
     gulp.start('scripts');
   });
 });
