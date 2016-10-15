4173a8b97edf91bd41b26b194b68e596a8f2751b
mooreorless <elliott.moore@moretonbay.qld.gov.au>
2016-10-15 19:45:22 +1000

    account, billing, help views

--- client/profile/profile.controller.js
+++ client/profile/profile.controller.js
@@ -19,6 +19,29 @@
 				$location.path('/');
         console.log(e);
       });
+
+	  vm.view = "Account";
+
+	  vm.setView = function(view) {
+	  	switch (view) {
+			  case 'Account':
+				  document.getElementById('account').className = "active";
+				  document.getElementById('billing').className = "";
+				  document.getElementById('help').className = "";
+				  break;
+			  case 'Billing':
+				  document.getElementById('billing').className = "active";
+				  document.getElementById('account').className = "";
+				  document.getElementById('help').className = "";
+				  break;
+			  case 'Help':
+				  document.getElementById('help').className = "active";
+				  document.getElementById('account').className = "";
+				  document.getElementById('billing').className = "";
+				  break;
+		  }
+		  vm.view = view;
+	  }
   }
 
 })();
